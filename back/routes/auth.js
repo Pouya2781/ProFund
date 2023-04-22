c7c27b146b84d0fc005b3d33b81f6762f0233601
Pouya2781 <pouya.s2781@gmail.com>
2023-04-22 17:13:44 +0330

    Initial commit for backend.
    This commit contains fundamental codes to start the server and also APIs
    needed to work with login / sign up page.

---
+++ back/routes/auth.js
@@ -0,0 +1,194 @@
+const request = require('request');
+const { VerificationCode, User } = require("../models");
+const config = require("config");
+const jwt = require("jsonwebtoken");
+const _ = require("lodash");
+const auth = require("../middleware/auth");
+const asyncMiddleware = require("../middleware/async");
+const { validatePhoneNumberData, validateVerificationData, validateUserData } = require("../validators/auth");
+const express = require("express");
+const { DATE } = require('sequelize');
+const router = express.Router();
+
+const smsTimeout = config.get("sms.timeout")
+const jwtPrivateKey = config.get("jwt.private_key");
+const smsUser = config.get("sms.user");
+const smsPass = config.get("sms.pass");
+const pattern1 = config.get("sms.pattern1");
+const pattern2 = config.get("sms.pattern2");
+
+router.get("/check", auth, (req, res) => {
+    res.status(200).send("User is already logged in!");
+});
+
+router.post("/number", asyncMiddleware(async (req, res) => {
+    const { error } = validatePhoneNumberData(req.body);
+    if (error) return res.status(400).json({ status: "validation_fail", message: error.details[0].message });
+
+    // Checking if user already exists
+    const user = await User.findOne({
+        where: {
+            phone_number: req.body.phone_number
+        }
+    });
+
+    const code = rng(100000, 999999);
+    // Sending verification code
+    if (user == null) {
+        await sendSms1(code, req);
+    } else {
+        await sendSms2(code, user.dataValues.full_name, req);
+    }
+
+    // const response = await fetch('http://ippanel.com/api/select', {
+    //     method: 'POST',
+    //     headers: {
+    //         'Content-Type': 'application/json'
+    //     },
+    //     body: JSON.stringify({
+    //         "op": "pattern",
+    //         "user": smsUser,
+    //         "pass": smsPass,
+    //         "fromNum": "3000505",
+    //         "toNum": req.body.phone_number,
+    //         "patternCode": pattern1,
+    //         "inputData": [
+    //             { "code": code }
+    //         ]
+    //     })
+    // });
+    // console.log(response.body);
+
+    // Removing previous entry
+    await VerificationCode.destroy({
+        where: { phone_number: req.body.phone_number },
+    });
+    // storing new entry in verification table in database
+    const verCode = await VerificationCode.create({ phone_number: req.body.phone_number, verification_code: code });
+
+    res.status(200).json({
+        data: _.pick(verCode, ["phone_number"]),
+        message: "Verification code successfully sent!",
+        status: "ok"
+    });
+}));
+
+router.post("/code", asyncMiddleware(async (req, res) => {
+    const { error } = validateVerificationData(req.body);
+    if (error) return res.status(400).json({ status: "validation_fail", message: error.details[0].message});
+
+    // Checking verification code
+    const entry = await VerificationCode.findOne({
+        where: {
+            phone_number: req.body.phone_number,
+            verification_code: req.body.code
+        }
+    });
+    if (entry == null) {
+        return res.status(400).json({ status: "invalid_code", message: "Invalid verification code!" });
+    }
+    const timeDifference = getTimeDifference(entry.dataValues.createdAt) / 1000;
+    console.log(timeDifference);
+    if (timeDifference > smsTimeout) {
+        return res.status(400).json({ status: "expired_code", message: "Verification code has been expired!" });
+    }
+
+    // Checking if user already exists
+    const user = await User.findOne({
+        where: {
+            phone_number: req.body.phone_number,
+        }
+    });
+    if (user == null) {
+        return res.status(200).json({ status: "need_sign_up", message: "Verification code is correct and user needs to sign up!"});
+    }
+
+    const token = jwt.sign({ phone_number: req.body.phone_number, role: "user" }, jwtPrivateKey);
+    res.set('x-auth-token', token);
+    res.status(200).json({ status: "ok", message: "Verification code is correct!" });
+}));
+
+router.post("/add", asyncMiddleware(async (req, res) => {
+    const { error } = validateUserData(req.body);
+    if (error) return res.status(400).json({ status: "validation_fail", message: error.details[0].message });
+
+    // Adding new user to database
+    let newUser;
+    try {
+        newUser = await User.create({
+            phone_number: req.body.phone_number,
+            full_name: req.body.full_name,
+            national_code: req.body.national_code,
+            email: req.body.email,
+            birth_date: req.body.birth_date
+        });
+    } catch (ex) {
+        return res.status(400).json({ status: "database_error", message: ex.errors[0].message });
+    }
+
+    const token = jwt.sign({ phone_number: req.body.phone_number, role: "user" }, jwtPrivateKey);
+    res.set('x-auth-token', token);
+    res.status(200).json({
+        data: _.pick(newUser, ["phone_number", "full_name", "national_code", "birth_date"]),
+        message: "New user added!",
+        status: "ok"
+    });
+}));
+
+function rng(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+async function sendSms1(code, req) {
+    await request.post({
+        url: 'http://ippanel.com/api/select',
+        body: {
+            "op": "pattern",
+            "user": smsUser,
+            "pass": smsPass,
+            "fromNum": "3000505",
+            "toNum": req.body.phone_number,
+            "patternCode": pattern1,
+            "inputData": [
+                { "code": code }
+            ]
+        },
+        json: true,
+    }, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+            console.log(response.body);
+        } else {
+            console.log(error);
+        }
+    });
+}
+
+async function sendSms2(code, name, req) {
+    await request.post({
+        url: 'http://ippanel.com/api/select',
+        body: {
+            "op": "pattern",
+            "user": smsUser,
+            "pass": smsPass,
+            "fromNum": "3000505",
+            "toNum": req.body.phone_number,
+            "patternCode": pattern2,
+            "inputData": [
+                { "code": code, "name": name }
+            ]
+        },
+        json: true,
+    }, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+            console.log(response.body);
+        } else {
+            console.log(error);
+        }
+    });
+}
+
+function getTimeDifference(timeStamp) {
+    return new Date().getTime() - new Date(timeStamp);
+}
+
+module.exports = router;
---
+++ back/validators/auth.js
@@ -0,0 +1,36 @@
+const Joi = require("joi");
+
+function validateUserData(body) {
+    const schema = Joi.object().keys({
+        phone_number: Joi.string().regex(/^\d{11}$/).required(),
+        full_name: Joi.string().min(3).max(30).required(),
+        email: Joi.string().min(5).max(30).email({ tlds: { allow: false } }).required(),
+        birth_date: Joi.string().regex(/^\d{4}-\d{2}-\d{2}$/).required(),
+        national_code: Joi.string().regex(/^\d{10}$/).required()
+    });
+
+    return schema.validate(body);
+}
+
+function validateVerificationData(body) {
+    const schema = Joi.object().keys({
+        phone_number: Joi.string().regex(/^\d{11}$/).required(),
+        code: Joi.string().regex(/^\d{6}$/).required()
+    });
+
+    return schema.validate(body);
+}
+
+function validatePhoneNumberData(body) {
+    const schema = Joi.object().keys({
+        phone_number: Joi.string().regex(/^\d{11}$/).required(),
+    });
+
+    return schema.validate(body);
+}
+
+module.exports = {
+    validatePhoneNumberData: validatePhoneNumberData,
+    validateVerificationData: validateVerificationData,
+    validateUserData: validateUserData
+}
