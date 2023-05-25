07eb64368fe1ca949196e77e912b6e2a9656b0ba
Pouya2781 <pouya.s2781@gmail.com>
2023-05-25 22:16:17 +0330

    Add API related to user and retrieving their related data.

---
+++ back/validators/user.js
@@ -0,0 +1,20 @@
+const Joi = require("joi");
+
+function validateUserVerificationData(body) {
+    const schema = Joi.object().keys({
+        full_name: Joi.string().min(3).max(30).required(),
+        email: Joi.string().min(5).max(30).email({ tlds: { allow: false } }).required(),
+        birth_date: Joi.string().regex(/^\d{4}-\d{2}-\d{2}$/).required(),
+        national_code: Joi.string().regex(/^\d{10}$/).required(),
+        state: Joi.string().min(3).max(20).required(),
+        city: Joi.string().min(3).max(20).required(),
+        address: Joi.string().min(3).max(120).required(),
+        bio: Joi.string().min(25).max(500).required(),
+    });
+
+    return schema.validate(body);
+}
+
+module.exports = {
+    validateUserVerificationData: validateUserVerificationData
+}
