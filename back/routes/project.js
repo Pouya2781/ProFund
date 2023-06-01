72a509fec0a5f4caf03df0843014d5d5110e0ee6
Mhadi80 <mohammadhadi.hoghooghi@gmail.com>
2023-06-01 12:52:12 +0330

    Search in project.

--- back/routes/project.js
+++ back/routes/project.js
@@ -23,6 +23,79 @@ router.get("/", auth, access, asyncMiddleware(async (req, res) => {
     });
 }));
 
+router.post("/", auth, access, asyncMiddleware(async (req, res) => {
+    const { error } = validateProjectSearchData(req.body);
+    if (error) return res.status(400).json({ status: "validation_fail", message: error.details[0].message });
 
+    let projects;
+    if (req.body.status !== undefined) {
+        projects = await Project.findAll({
+            include: [
+                {
+                    model: ProjectDescription,
+                    where: {
+                        ...(req.body.title && { title: { [Op.like]: `%${req.body.title}%` } }),
+                        ...(req.body.subtitle && { subtitle: { [Op.like]: `%${req.body.subtitle}%` } })
+                    }
+                }
+            ],
+            where: {
+                ...(req.body.goal && { goal: { [Op.like]: `%${req.body.goal}%` } }),
+                ...(req.body.category && { category: { [Op.like]: `%${req.body.category}%` } }),
+                ...(req.body.investedAmount && { investedAmount: { [Op.like]: `%${req.body.investedAmount}%` } }),
+                ...(req.body.investorCount && { investorCount: { [Op.like]: `%${req.body.investorCount}%` } }),
+                ...(req.body.status && { status: req.body.status }),
+                ...(req.body.hasDonate !== undefined && { hasDonate: req.body.hasDonate }),
+                ...(req.body.hasToken !== undefined && { hasToken: req.body.hasToken })
+            },
+            raw: true
+        });
+    } else {
+        projects = await Project.findAll({
+            include: [
+                {
+                    model: ProjectDescription,
+                    where: {
+                        ...(req.body.title && { title: { [Op.like]: `%${req.body.title}%` } }),
+                        ...(req.body.subtitle && { subtitle: { [Op.like]: `%${req.body.subtitle}%` } })
+                    }
+                }
+            ],
+            where: {
+                ...(req.body.goal && { goal: { [Op.like]: `%${req.body.goal}%` } }),
+                ...(req.body.category && { category: { [Op.like]: `%${req.body.category}%` } }),
+                ...(req.body.investedAmount && { investedAmount: { [Op.like]: `%${req.body.investedAmount}%` } }),
+                ...(req.body.investorCount && { investorCount: { [Op.like]: `%${req.body.investorCount}%` } }),
+                ...(req.body.status && { status: req.body.status }),
+                ...(req.body.hasDonate !== undefined && { hasDonate: req.body.hasDonate }),
+                ...(req.body.hasToken !== undefined && { hasToken: req.body.hasToken }),
+                status: ['active', 'pending_payment', 'pending_delivery']
+            },
+            raw: true
+        });
+    }
+    
+
+    res.status(200).json({
+        data: _.map(projects, (item) => {
+            return {
+                id: item['id'],
+                userId: item['userId'],
+                goal: item['goal'],
+                category: item['category'],
+                investedAmount: item['investedAmount'],
+                investorCount: item['investorCount'],
+                hasDonate: item['hasDonate'],
+                hasToken: item['hasToken'],
+                status: item['status'],
+                expirationDate: item['expirationDate'],
+                title: item['ProjectDescription.title'],
+                subtitle: item['ProjectDescription.subtitle']
+            }
+        }),
+        message: "Project list retrieved successfully!",
+        status: "ok"
+    });
+}));
 
 module.exports = router;
