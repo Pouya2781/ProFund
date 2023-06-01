050e91dea5627ea8bd899d9c221a5908e9b882e7
Mhadi80 <mohammadhadi.hoghooghi@gmail.com>
2023-06-01 18:07:59 +0330

    Add comment and reply APIs.

--- back/routes/project.js
+++ back/routes/project.js
@@ -170,4 +170,57 @@ router.post("/tokens", auth, access, asyncMiddleware(async (req, res) => {
     });
 }));
 
+router.post("/comment", auth, access, asyncMiddleware(async (req, res) => {
+    const { error } = validateIdData(req.body);
+    if (error) return res.status(400).json({ status: "validation_fail", message: error.details[0].message });
+
+    const comments = await Comment.findAll({
+        include: [User],
+        where: {
+            projectId: req.body.id
+        },
+        raw: true
+    });
+
+    res.status(200).json({
+        data: _.map(comments, (item) => {
+            return {
+                userId: item['userId'],
+                message: item['message'],
+                projectId: item['projectId'],
+                createdAt: item['createdAt'],
+                fullName: item['User.fullName']
+            }
+        }),
+        message: "Project comments retrieved successfully!",
+        status: "ok"
+    });
+}));
+
+router.post("/reply", auth, access, asyncMiddleware(async (req, res) => {
+    const { error } = validateIdData(req.body);
+    if (error) return res.status(400).json({ status: "validation_fail", message: error.details[0].message });
+
+    const replies = await Reply.findAll({
+        include: [User],
+        where: {
+            commentId: req.body.id
+        }
+    });
+
+    res.status(200).json({
+        data: _.map(replies, (item) => {
+            return {
+                userId: item['userId'],
+                message: item['message'],
+                commentId: item['commentId'],
+                createdAt: item['createdAt'],
+                fullName: item['User.fullName']
+            }
+        }),
+        message: "Comment replies retrieved successfully!",
+        status: "ok"
+    });
+}));
+
 module.exports = router;
