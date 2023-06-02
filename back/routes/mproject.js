9acb06e29aa4ee8fbdb34c945c037dad2fa2200c
c0d3z <16648282+c0d3z@users.noreply.github.com>
2023-06-02 16:16:08 +0330

    fix bug

--- back/routes/mproject.js
+++ back/routes/mproject.js
@@ -228,57 +228,61 @@ router.post("/step3", auth, access, asyncMiddleware(async (req, res) => {
   logger.info('Step 3 completed successfully!');
 }));
 
-// Step 4: Display project information and confirm
-router.post("/step4", auth, access, asyncMiddleware(async (req, res) => {
-  // Retrieve the project information from the database
-  const project = await Project.findOne({ where: { id: new_project_id } });
-
-  // Retrieve the project description from the database
-  const projectDescription = await ProjectDescription.findOne({ where: { projectId: new_project_id } });
-
-  // Retrieve the token details from the database
-  const token = await Token.findOne({ where: { id: new_token_id } });
-
-  // Combine the retrieved information into a single object
-  const data = {
-    project: project ? project.toJSON() : null,
-    projectDescription: projectDescription ? projectDescription.toJSON() : null,
-    token: token ? token.toJSON() : null,
-  };
-
-    // update project status
-    try{
-      await Project.update({
-        status:"pending approval"
-      }, {
-        where:{
-          id: new_project_id
-        }
-      }
-      );
-      logger.info('Project status updated successfully!(step5)');
-    } catch (ex) {
-      return res.status(400).json({ status: "database_error", message: ex.errors[0].message });
+// Step 4: adding project confirmation
+router.get("/step4", auth, access, asyncMiddleware(async (req, res) => {
+  try {
+    // Retrieve the project information from the database
+    const project = await Project.findOne({ where: { id: new_project_id } });
+    if (!project) {
+      logger.error(`Project not found for ID: ${new_project_id}`);
+      return res.status(404).json({ status: "not_found", message: "Project not found" });
     }
+    logger.info(`Retrieved project with ID: ${new_project_id}`);
 
-  res.status(200).json({
-    data,
-    message: "Step 4 completed successfully!",
-    status: "ok",
-  });
-  logger.info('Step 4 completed successfully!');
+    // Retrieve the project description from the database
+    const projectDescription = await ProjectDescription.findOne({ where: { projectId: new_project_id } });
+    if (!projectDescription) {
+      logger.error(`Project description not found for project ID: ${new_project_id}`);
+      return res.status(404).json({ status: "not_found", message: "Project description not found" });
+    }
+    logger.info(`Retrieved project description for project ID: ${new_project_id}`);
+
+    // Retrieve the token details from the database
+    const token = await Token.findOne({ where: { id: new_token_id } });
+    if (!token) {
+      logger.error(`Token not found for ID: ${new_token_id}`);
+      return res.status(404).json({ status: "not_found", message: "Token not found" });
+    }
+    logger.info(`Retrieved token with ID: ${new_token_id}`);
+
+    // Combine the retrieved information into a single object
+    const data = {
+      project: project ? project.toJSON() : null,
+      projectDescription: projectDescription ? projectDescription.toJSON() : null,
+      token: token ? token.toJSON() : null,
+    };
+
+    // Update project status to "pending approval"
+    await Project.update({ status: "pending approval" }, { where: { id: new_project_id } });
+    logger.info(`Updated project status to 'pending approval' for project ID: ${new_project_id}`);
+
+    res.status(200).json({
+      data,
+      message: "Step 4 completed successfully!",
+      status: "ok",
+    });
+    logger.info('Step 4 completed successfully!');
+  } catch (error) {
+    logger.error(`An error occurred in Step 4: ${error.message}`);
+    return res.status(500).json({ status: "error", message: "An error occurred during Step 4" });
+  }
 }));
 
 
 // Step 5: Finalize project
-router.post("/step5", auth, access, asyncMiddleware(async (req, res) => {
-  // update project status
-  
-  
-
-  // Send the confirmation message and the created project in the response
+router.get("/step5", auth, access, asyncMiddleware(async (req, res) => {
+  // Send the confirmation message in the response
   res.status(200).json({
-    data,
     message: "Project added successfully!",
     status: "ok",
   });
